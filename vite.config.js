370b2ebe63bf9961919d1efd7b777dac39295d85
Shashank696-sys <pandeyshashank039@gmail.com>
2025-07-10 03:13:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,              // Accept all hosts, including 0.0.0.0 and mobile IPs
-    port: 5173,              // Your dev port
-    strictPort: true,
-    https: false,            // Keep false unless you're serving with HTTPS directly
-    cors: true, 
-    allowedHosts: ["lock-watts-disease-andrew.trycloudflare.com"], //Frontend link
-    headers: {
-      'Access-Control-Allow-Origin': '*',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
   },
 });
