6acf8277f68500ef279d4ad88a00c4c1ff7b1541
Shashank696-sys <pandeyshashank039@gmail.com>
2025-07-10 03:06:58 +0530

    Update Settings.jsx

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import './Settings.css';
+import './temp.css';
 
 const Settings = ({ onClose }) => {
   const [gmail, setGmail] = useState("");
